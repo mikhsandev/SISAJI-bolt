ff85aeeac3e77967c609f361a58bc38865e415a1
Muhammad Ikhsan <mikhsandev@gmail.com>
2024-06-13 12:02:45 +0700

    Style form confirmation message

--- resources/views/themes/zeus/bolt/submitted.blade.php
+++ resources/views/themes/zeus/bolt/submitted.blade.php
@@ -1,20 +1,21 @@
 <div>
-    <div class="max-w-4xl mx-auto px-4">
+    <div class="text-center">
         <x-filament::section>
-            @if(!empty($zeusForm->options['confirmation-message']))
-                <span class="text-md text-gray-600">
-                    {!! $zeusForm->options['confirmation-message'] !!}
-                </span>
-            @else
-                <span class="text-md text-gray-600">
-                    {{ __('the form') }}
-                    <span class="font-semibold">{{ $zeusForm->name ?? '' }}</span>
-                    {{ __('submitted successfully') }}.
-                </span>
-            @endif
+            <div class="bg-blue-100 p-6" style="margin: -25px">
+                @if(!empty($zeusForm->options['confirmation-message']))
+                    <span class="text-md text-blue-100">
+                        {!! $zeusForm->options['confirmation-message'] !!}
+                    </span>
+                @else
+                    <span class="text-md text-blue-100">
+                        {{ __('the form') }}
+                        <span class="font-semibold">{{ $zeusForm->name ?? '' }}</span>
+                        {{ __('submitted successfully') }}.
+                    </span>
+                @endif
 
-            {!! \LaraZeus\Bolt\Facades\Extensions::init($zeusForm, 'SubmittedRender', ['extensionData' => $extensionData['extInfo']['itemId'] ?? 0]) !!}
-
+                {!! \LaraZeus\Bolt\Facades\Extensions::init($zeusForm, 'SubmittedRender', ['extensionData' => $extensionData['extInfo']['itemId'] ?? 0]) !!}
+            </div>
         </x-filament::section>
     </div>
 </div>
